5bf0b2c4bb605d6bf78950bbe89d49b61a5f8b84
Pankaj Chohan <44444897+PankajChohan9820@users.noreply.github.com>
2023-09-12 11:20:27 -0700

    Update get_links.py

--- helpers/get_links.py
+++ helpers/get_links.py
@@ -2,17 +2,23 @@ from bs4 import BeautifulSoup as bs
 import time
 import helper
 import warnings
+from typing import List
+
+# Ignore warnings
 warnings.filterwarnings("ignore")
 
-warnings.filterwarnings("ignore")
+def product_links() -> List[str]:
+    """
+    Scrapes product links from the Vestiaire Collective website.
 
-def product_links():
+    Returns:
+    - List[str]: List of product URLs.
+    """
     driver = helper.get_webdriver()
     # Get the list of all products
     product_links = []
     try:
-        i = 1
-        for page in range(1,3):
+        for page in range(1, 3):  # Scraping pages 1 and 2
             if page == 1:
                 url = 'https://us.vestiairecollective.com/women-bags/'
             else:
@@ -31,13 +37,11 @@ def product_links():
             product_ul = soup.find("ul", class_="product-search_catalog__flexContainer__Dg0eL")
             if product_ul:
                 products = product_ul.find_all("li")
-            for j in range(len(products)):
-                p = products[j]
-                link = p.findAll('a')
-                pages = link[0]['href']
-                product_links.append(pages)
-            i += 1
+                for j in range(len(products)):
+                    p = products[j]
+                    link = p.findAll('a')
+                    pages = link[0]['href']
+                    product_links.append(pages)
         return product_links
     except Exception as e:
-        return []
-        # print(str(e))
+        return []  # Return an empty list in case of an error
